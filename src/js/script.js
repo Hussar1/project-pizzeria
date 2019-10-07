2165db9dc01d3a02af6abdcdd0813c04185ffaa8
Przemek Drożniak <mr.shemeck@gmail.com>
2019-10-07 14:25:48 +0200

    Fill processOrder() within Product, console sees price changes while HTML .price will not change

--- src/js/script.js
+++ src/js/script.js
@@ -138,6 +138,39 @@
     processOrder(){
       const thisProduct = this;
       console.log('thisProduct of processOrder():', thisProduct);
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData:', formData);
+      /* set variable price to equal thisProduct.data.price */
+      let totalPrice = thisProduct.data.price;
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in thisProduct.data.params) {
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const param = thisProduct.data.params[paramId];
+        /* START LOOP: for each optionId in param.options */
+        for (let optionId in param.options){
+          /* save the element in param.options with key optionId as const option */
+          const option = param.options[optionId];
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+          /* START IF: if option is selected and option is not default */
+          if (optionSelected && !option.default){
+            /* add price of option to variable price */
+            totalPrice += option.price;
+          /* END IF: if option is selected and option is not default */
+          }
+          /* START ELSE IF: if option is not selected and option is default */
+          else if (!optionSelected && option.default){
+            /* deduct price of option from price */
+            totalPrice -= option.price;
+          /* END ELSE IF: if option is not selected and option is default */
+          }
+        /* END LOOP: for each optionId in param.options */
+        }
+      /* END LOOP: for each paramId in thisProduct.data.params */
+      }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      console.log('totalPrice:', totalPrice);
+      thisProduct.priceElem = totalPrice;
     }
   }
 
