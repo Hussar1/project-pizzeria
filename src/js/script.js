d1226b6bfa831f8db0830ed6e05ee5fb8e53b32b
Przemek Drożniak <mr.shemeck@gmail.com>
2019-10-09 13:53:30 +0200

    Add initActions method with its call within Cart, allowing to display/hide cart and its contents

--- src/js/script.js
+++ src/js/script.js
@@ -291,6 +291,7 @@
       const thisCart = this;
       thisCart.products = [];
       thisCart.getElements(element);
+      thisCart.initActions();
       console.log('new Cart:', thisCart);
     }
 
@@ -298,6 +299,14 @@
       const thisCart = this;
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function (){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
